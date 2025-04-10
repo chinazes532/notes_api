e0eedabf6ee94495fbd0f7361eca65b4469e74a4
chinazes532 <chernikof50@gmail.com>
2025-04-10 18:04:50 +0300

    Добавление таблицы users

--- src/db/models.py
+++ src/db/models.py
@@ -1,10 +1,19 @@
 from typing import Annotated
 
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from src.db.database import Base
 
 intpk = Annotated[int, mapped_column(primary_key=True)]
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[intpk]
+    name: Mapped[str] = mapped_column(String(100))
+    email: Mapped[str] = mapped_column(String(100), unique=True)
+    password: Mapped[str] = mapped_column(String(100))
 
 
 class Note(Base):
@@ -13,3 +22,4 @@ class Note(Base):
     id: Mapped[intpk]
     title: Mapped[str] = mapped_column(String(100))
     desc: Mapped[str] = mapped_column(String(1000), nullable=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)
